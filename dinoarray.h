99d0ef0a97bbf49b5e52116885b0171bc28b6470
CasterraDev <casterradev@gmail.com>
2025-02-26 15:48:09 -0500

    Added Doc Comments

--- dinoarray.h
+++ dinoarray.h
@@ -27,44 +27,89 @@ void* _dino_insert_at(void* array, unsigned long long idx, void* valuePtr);
 #define DINO_DEFAULT_SIZE 1
 #define DINO_DEFAULT_RESIZE_FACTOR 2
 
-// Define function wrappers
+//====================== Define function wrappers ======================
+
+/**
+ *  Create a Dino array. Initial size will be 1. For custom size look at `dinoCreateReserve()` 
+ */
 #define dinoCreate(type) _dino_create(DINO_DEFAULT_SIZE, sizeof(type));
 
+/**
+ *  Create a Dino array with a custom initial length.
+ *  So if you know you need 32 elements you should do dinoCreateReserve(32, [type]).
+ *  This saves on having to remalloc a ton of times.
+ */
 #define dinoCreateReserve(length, type) _dino_create(length, sizeof(type));
 
+/**
+ *  Frees the Dino array
+ */
 #define dinoDestroy(array) _dino_destroy(array);
 
+/**
+ *  Shrinks the Dino array to it's length so no memory is being wasted.
+ *  This does perform a reallocate.
+ */
 #define dinoShrink(array) _dino_shrink(array);
 
+/**
+ *  Push an element value to the Dino array. Will automatically resize
+ */
 #define dinoPush(array, value)                                                 \
     {                                                                          \
         typeof(value) t = value;                                               \
         array = _dino_push(array, &t);                                         \
     }
 
+/**
+ *  Pop the last element value from the Dino array.
+ */
 #define dinoPop(array, valuePtr) _dino_pop(array, valuePtr)
 
+/**
+ *  Insert an element value to the Dino array at a certain index. Will automatically resize
+ */
 #define dinoInsertAt(array, index, value)                                      \
     {                                                                          \
         typeof(value) temp = value;                                            \
         array = _dino_insert_at(array, index, &temp);                          \
     }
 
+/**
+ *  Pop the element value with the index from the Dino array.
+ */
 #define dinoPopAt(array, index, value_ptr) _dino_pop_at(array, index, value_ptr)
 
-// QOL Functions Defined
+//====================== QOL Functions Defined ======================
 
+/**
+ *  Clears the Dino array.
+ *  Does NOT do any reallocating. So the memory will still be malloced.
+ */
 #define dinoClear(array) _dino_field_set(array, DINOARRAY_LENGTH, 0)
 
+/**
+ *  Sets the length of the Dino Array.
+ *  Made to be a helper function for other DinoArray functions. Users be careful.
+ */
 #define dinoLengthSet(array, value)                                            \
     _dino_field_set(array, DINOARRAY_LENGTH, value)
 
-// QOL Variables Defined
+//====================== QOL Variables Defined ====================== 
 
+/**
+ *  Get the Max Size / Capacity of the Dino Array
+ */
 #define dinoMaxSize(array) _dino_field_get(array, DINOARRAY_MAX_SIZE)
 
+/**
+ *  Get the Length of the Dino Array
+ */
 #define dinoLength(array) _dino_field_get(array, DINOARRAY_LENGTH)
 
+/**
+ *  Get the Stride of the Dino Array
+ */
 #define dinoStride(array) _dino_field_get(array, DINOARRAY_STRIDE)
 
 #ifdef DINO_IMPLEMENTION
